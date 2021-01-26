548f47075647b849870505251625d90da9cfca32
Jordan Tranchina <Jordan12522@gmail.com>
2021-01-26 16:31:50 -0600

    completed lab with bonus points!

--- lab/week4.js
+++ lab/week4.js
@@ -10,14 +10,35 @@ function renderProduct(product) {
     `
     <div class="p-4 w-full md:w-1/2 lg:w-1/3">
       <div class="border h-full p-4 flex flex-col">
-        <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+        <h2 class="text-lg font-bold mb-4">
+          ${product.name}
+        </h2>
         <div class="mb-4"><img
-            src=${product.image}>
+          src=${product.image}>
         </div>
         <div class="mb-4 text-gray-900">
           ${product.description}
         </div>
-        <div class="mt-auto text-purple-500 text-2xl">$5.49</div>
+        <div class="mt-auto text-purple-500 text-2xl">
+          ${product.price}
+        </div>
+      </div>
+    </div>
+    `
+  )
+}
+function renderPromotion(promotion) {
+  let outputElement = document.querySelector(".products")
+  outputElement.insertAdjacentHTML("beforeend",
+    `
+    <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+      <div class="border h-full p-4 flex flex-col">
+        <h2 class="text-lg font-bold mb-4">
+          ${promotion.type}
+        </h2>
+        <div class="mb-4 text-gray-900">
+          ${promotion.description}
+        </div>
       </div>
     </div>
     `
@@ -31,8 +52,14 @@ async function pageLoaded() {
 
   for (let i = 0; i < json.products.length; i++) {
     let product = json.products[i]
-    console.log(product);
+    // console.log(product);
     renderProduct(product)
+  }
+
+  for (let i = 0; i < json.promotions.length; i++) {
+    let promotion = json.promotions[i]
+    // console.log(promotion);
+    renderPromotion(promotion)
   }
 
 }
