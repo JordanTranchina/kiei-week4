6249d4720c563a018ddcc051ddba54ed1b21b3ec
Jordan Tranchina <Jordan12522@gmail.com>
2021-01-26 16:10:35 -0600

    prepping for bonus points

--- lab/week4.js
+++ lab/week4.js
@@ -4,14 +4,6 @@
 // You can visit the provided URL – https://kiei451.com/api/products.json –
 // in Chrome to see what the data looks like.
 
-// Write a loop to iterate through the product data; 
-// each iteration of the loop should call a function called renderProduct, which accepts an object representing
-// a single product object as input, and appends HTML to an existing element on the page
-// with the class "products". You can use the existing HTML in the "products" element
-// as a template, deleting it when you're finished.
-
-// 🔥 define your renderProduct function here
-
 function renderProduct(product) {
   let outputElement = document.querySelector(".products")
   outputElement.insertAdjacentHTML("beforeend",
@@ -33,19 +25,9 @@ function renderProduct(product) {
 }
 
 
-
-
 async function pageLoaded() {
   let response = await fetch('https://kiei451.com/api/products.json')
   let json = await response.json()
-
-  // writes the returned JSON to the console
-  // console.log(json)
-
-  // 🔥 start here: write code to loop through the products; each
-  // iteration of the loop should call your renderProduct function
-  // make it work first; then extract to the separate renderProduct
-  // function after it's 100% working without it
 
   for (let i = 0; i < json.products.length; i++) {
     let product = json.products[i]
