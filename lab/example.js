a7734e793ebec338bd082d5bdf18e89b0915f773
Jordan Tranchina <Jordan12522@gmail.com>
2021-01-26 16:08:30 -0600

    Usabe lab result

---
+++ lab/example.js
@@ -0,0 +1,24 @@
+{
+    promotions: [
+        {
+            type: "discount",
+            description: "Buy 2 pairs of drum sticks and get a third pair for free!"
+        },
+        {
+            type: "contest",
+            description: "Enter to win a $100 gift card on us!"
+        },
+        {
+            type: "shipping",
+            description: "Spend $100 and get free shipping site-wide!"
+        },
+    ],
+        products: [
+            {
+                name: "Alto Saxophone Reeds (Box of 3)",
+                image: "https://images.unsplash.com/photo-1415201364774-f6f0bb35f28f?ixlib=rb-1.2.1&auto=format&fit=crop&w=640&q=80",
+                price: 9.99,
+                description: "High-quality reeds designed for a wide variety of playing situations, these are cut from the most flexible grade of cane."
+            },
+        ]
+}
--- lab/week4.js
+++ lab/week4.js
@@ -4,25 +4,54 @@
 // You can visit the provided URL – https://kiei451.com/api/products.json –
 // in Chrome to see what the data looks like.
 
-// Write a loop to iterate through the product data; each iteration of the loop
-// should call a function called renderProduct, which accepts an object representing
+// Write a loop to iterate through the product data; 
+// each iteration of the loop should call a function called renderProduct, which accepts an object representing
 // a single product object as input, and appends HTML to an existing element on the page
 // with the class "products". You can use the existing HTML in the "products" element
 // as a template, deleting it when you're finished.
 
 // 🔥 define your renderProduct function here
 
+function renderProduct(product) {
+  let outputElement = document.querySelector(".products")
+  outputElement.insertAdjacentHTML("beforeend",
+    `
+    <div class="p-4 w-full md:w-1/2 lg:w-1/3">
+      <div class="border h-full p-4 flex flex-col">
+        <h2 class="text-lg font-bold mb-4">${product.name}</h2>
+        <div class="mb-4"><img
+            src=${product.image}>
+        </div>
+        <div class="mb-4 text-gray-900">
+          ${product.description}
+        </div>
+        <div class="mt-auto text-purple-500 text-2xl">$5.49</div>
+      </div>
+    </div>
+    `
+  )
+}
+
+
+
+
 async function pageLoaded() {
   let response = await fetch('https://kiei451.com/api/products.json')
   let json = await response.json()
 
   // writes the returned JSON to the console
-  console.log(json)
+  // console.log(json)
 
   // 🔥 start here: write code to loop through the products; each
   // iteration of the loop should call your renderProduct function
   // make it work first; then extract to the separate renderProduct
   // function after it's 100% working without it
+
+  for (let i = 0; i < json.products.length; i++) {
+    let product = json.products[i]
+    console.log(product);
+    renderProduct(product)
+  }
 
 }
 
